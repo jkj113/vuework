971c3fe3b9812d01203ac36286eb8a5db5032f09
jkj113 <kumjoo_92@naver.com>
2020-01-20 15:53:38 +0900

    vue-todo-app first
    testtest

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" @click="clearTodo">Clear All-test2</span>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        clearTodo: function() {
+            localStorage.clear();
+        }
+    }
+}
+</script>
+
+<style scoped>
+.clearAllContainer{
+    width: 8.5rem;
+    height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+}
+
+.clearAllBtn{
+    color: #e20303;
+    display: block;
+}
+</style>
